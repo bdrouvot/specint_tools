5d453d4390f5c5d215a85deb2300653553f498cc
bdt <bdt@bdt.mail>
2017-04-30 13:10:19 +0200

    all good

--- gen_text_specint.sh
+++ gen_text_specint.sh
@@ -6,5 +6,4 @@ textutil -convert txt "specint2006.html"
 awk '{if($1~/^HTML/){l1=$0;getline;l2=(l1";"$0);getline;l3=(l2";"$0);getline;l4=(l3";"$0);getline;l5=(l4";"$0);getline;l6=(l5";"$0);getline;l7=(l6";"$0);getline;print l7";"$0" "p;p=""} else {if($1~/^Peak/){p=""} else {p=(p";"$0)}}}'  specint2006.txt > specint1.txt
 awk '{split($0,a,"HTML");$1=a[1] ; $2=a[2]; print $2,$1}' specint1.txt > specint2.txt
 awk -F';' '{print $8/$3";", $0}' specint2.txt > specint.txt
-awk -F';' '{gsub(" ","\\ ",$NF);gsub("\,","\\,",$NF);print "specint_meas,cpu_type="$NF" specval="$1}' specint.txt
 rm specint2006.html specint2006.txt specint1.txt specint2.txt 
